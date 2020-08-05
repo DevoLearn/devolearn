7212de7ef6b43f70cb38b029102ce14a8bde6a24
mayukhdeb <mayukhmainak2000@gmail.com>
2020-08-05 13:03:11 +0530

    setup: modify install_requires list

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="develearn",
+    name="devolearn",
     version="0.0.1",
     author="Mayukh Deb, Ujjwal Singh, Bradley Alicea", #To be decided, also there can be name of "DevoWorm | OpenWorm Foundation"
     author_email="mayukhmainak2000@gmail.com, ujjwal18113@iiitd.ac.in, balicea@openworm.org", #Subject to change, we can also use Devolearn official Email address.
@@ -19,22 +19,32 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
 
-
-
-
-    #Mayukh, please run pip freeze command to get all the dependencies required and any if missing. Also delete this comment once done.
-
     install_requires=[
-          "Pillow",
-          "joblib",
-          "matplotlib",
-          "numpy",
-          "opencv-python",
-          "pandas",
-          "torch",
-          "torchvision",
-          "tqdm"
-          "segmentation-models-pytorch"
+        "torch",
+        "torchvision",
+        "segmentation-models-pytorch",
+        "scikit-learn",
+        "scipy",
+        "sklearn",
+        "matplotlib",
+        "numpy",
+        "pretrainedmodels",
+        "opencv-python",
+        "pandas",
+        "tqdm",
+        "cycler",
+        "efficientnet-pytorch",
+        "future",
+        "imutils",
+        "joblib",
+        "kiwisolver",
+        "munch",
+        "Pillow",
+        "pyparsing",
+        "python-dateutil",
+        "pytz",
+        "six",
+        "threadpoolctl"
       ],
     python_requires='>=3.6',      
 )
