90b4a95a1fe6a5a20d117f18318e6e7fedb9f152
mayukhdeb <mayukhmainak2000@gmail.com>
2020-08-06 17:24:44 +0530

    setup: move to version 0.1.9

--- devolearn/__init__.py
+++ devolearn/__init__.py
@@ -2,7 +2,7 @@ from .lineage_population_model import *
 from .embryo_generator_model import *
 from .embryo_segmentor import *
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 
 __all__ = ["lineage_populaton_model",
             "embryo_generator_model",
