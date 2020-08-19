4377c8e3d6d2ea0f16d115b3169ff42ad33e64e6
mayukhdeb <mayukhmainak2000@gmail.com>
2020-08-19 13:16:07 +0530

    __init__: add test imports

--- devolearn/__init__.py
+++ devolearn/__init__.py
@@ -1,9 +1,12 @@
 from .lineage_population_model import *
 from .embryo_generator_model import *
 from .embryo_segmentor import *
+from .tests import test ## tests/test.py/<class test()>
 
 __version__ = "0.1.9"
 
+## folder names below
 __all__ = ["lineage_populaton_model",
             "embryo_generator_model",
-            "embryo_segmentor"]
+            "embryo_segmentor",
+            "tests"]
