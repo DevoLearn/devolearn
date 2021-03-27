944c3543d08c004b6792043f75eb28f9b6999a32
aswarth123 <c.v.aswarth@gmail.com>
2021-03-27 22:56:01 +0530

    embryo_generator_model.py: migrate cv2 operations to imageio

--- devolearn/embryo_generator_model/embryo_generator_model.py
+++ devolearn/embryo_generator_model/embryo_generator_model.py
@@ -9,6 +9,7 @@ import torchvision.models as models
 
 import os
 import cv2
+import imageio
 import wget
 from tqdm import tqdm, tqdm_notebook
 from PIL import Image
@@ -22,7 +23,7 @@ from ..base_inference_engine import InferenceEngine
 
 
 """
-GAN to generate images of embryos 
+GAN to generate images of embryos
 """
 
 class Generator(nn.Module):
@@ -70,16 +71,16 @@ class Generator(nn.Module):
         return self.main(input)
 
 
-class embryo_generator_model(InferenceEngine):   
+class embryo_generator_model(InferenceEngine):
     def __init__(self, device = "cpu"):
         """Generate synthetic single or multiple images of embryos.
-        Do not tweak these unless you're changing the Generator() with a new model with a different architecture. 
+        Do not tweak these unless you're changing the Generator() with a new model with a different architecture.
 
         Args:
             device (str, optional): set to "cuda", runs operations on gpu and set to "cpu", runs operations on cpu. Defaults to "cpu".
         """
         self.device = device
-        self.ngf = 128 ## generated image size 
+        self.ngf = 128 ## generated image size
         self.nz = 128
         self.nc = 1
         self.generator = Generator(self.ngf, self.nz, self.nc)
@@ -99,17 +100,17 @@ class embryo_generator_model(InferenceEngine):
             filename = wget.download(self.model_url, out= self.model_dir)
             # print(filename)
             self.generator.load_state_dict(torch.load(self.model_dir + "/" + self.model_name, map_location= self.device))
-        
+
         self.generator.to(self.device)
 
 
-        
-    
+
+
 
 
     def generate(self, image_size = (700,500)):
         """Generate one synthetic image of embryo.
-        The native size of the GAN's output is 256*256, and then it resizes the generated image to the desired size. 
+        The native size of the GAN's output is 256*256, and then it resizes the generated image to the desired size.
         reference{
             https://github.com/DevoLearn/devolearn#generating-synthetic-images-of-embryos-with-a-pre-trained-gan
         }
@@ -118,19 +119,19 @@ class embryo_generator_model(InferenceEngine):
             image_size (tuple, optional): size of generated image,(width,height). Defaults to (700,500).
 
         Returns:
-            np.array : 1 channel image 
+            np.array : 1 channel image
         """
         with torch.no_grad():
             noise = torch.randn([1,128,1,1]).to(self.device)
             im = self.generator(noise)[0][0].cpu().detach().numpy()
         im = cv2.resize(im, image_size)
-        im = 255 - cv2.convertScaleAbs(im, alpha=(255.0))   ## temporary fix against inverted images 
+        im = 255 - cv2.convertScaleAbs(im, alpha=(255.0))   ## temporary fix against inverted images
 
         return im
 
 
     def generate_n_images(self, n = 3, foldername = "generated_images", image_size = (700,500), notebook_mode = False):
-        """This is an extension of the generator.generate() function for generating multiple images at once and saving them into a folder. 
+        """This is an extension of the generator.generate() function for generating multiple images at once and saving them into a folder.
         reference{
             https://github.com/DevoLearn/devolearn#generating-synthetic-images-of-embryos-with-a-pre-trained-gan
         }
@@ -147,12 +148,12 @@ class embryo_generator_model(InferenceEngine):
         if notebook_mode == True:
             for i in tqdm_notebook(range(n), desc = "generating images :"):
                 filename = foldername + "/" + str(i) + ".jpg"
-                gen_image = self.generate()  ## 2d numpy array 
-                cv2.imwrite(filename, gen_image)
+                gen_image = self.generate()  ## 2d numpy array
+                imageio.imwrite(filename, gen_image)
         else:
             for i in tqdm(range(n), desc = "generating images :"):
                 filename = foldername + "/" + str(i) + ".jpg"
-                gen_image = self.generate()  ## 2d numpy array 
-                cv2.imwrite(filename, gen_image)
+                gen_image = self.generate()  ## 2d numpy array
+                imageio.imwrite(filename, gen_image)
 
         print ("Saved ", n, " images in", foldername)
